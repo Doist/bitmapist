b368eb175bfdf9ae0326a04f981e54e718a9e082
Roman Imankulov <roman.imankulov@gmail.com>
2014-10-30 17:31:20 +0300

    Updated version to 3.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='bitmapist',
-      version = '3.4',
+      version = '3.5',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
