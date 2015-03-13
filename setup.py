ca4f3872ecf15604200d8fbd7c6fb5df50200673
Roman Imankulov <roman.imankulov@gmail.com>
2015-03-13 12:28:04 +0300

    Updated version to 3.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='bitmapist',
-      version = '3.7',
+      version = '3.8',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
