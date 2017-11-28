edafd2b545963336e120989b5c437884c6343598
Roman Imankulov <roman.imankulov@gmail.com>
2017-11-28 14:53:14 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='bitmapist',
-      version='3.99',
+      version='3.100',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
