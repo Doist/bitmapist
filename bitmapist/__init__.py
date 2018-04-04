Doist/bitmapist

commit 2ab8e162339db79529ad6b10fb863ac4a249c6a5

Add python 3 compatibility

bitmapist/__init__.py
@@ -79,6 +79,8 @@ Additionally you can supply an extra argument to mark_event to bypass the defaul
 :developer: Amir Salihefendic ( http://amix.dk )
 :license: BSD
 """
+from builtins import range, bytes
+
 import redis
 import calendar
 from datetime import datetime, date, timedelta
@@ -182,6 +184,7 @@ def get_event_names(system='default', prefix='', batch=10000):
     expr = 'trackist_%s*' % prefix
     ret = set()
     for result in cli.scan_iter(match=expr, count=batch):
+        result = result.decode()
         chunks = result.split('_')
         event_name = '_'.join(chunks[1:-1])
         if not event_name.startswith('bitop_'):
@@ -221,13 +224,15 @@ class MixinIter:
         if val is None:
             return
 
+        val = bytes(val)
+
         zero = chr(0)
         for char_num, char in enumerate(val):
             # shortcut
             if char == zero:
                 continue
             # find set bits, generate smth like [1, 0, ...]
-            bits = [(ord(char) >> i) & 1 for i in xrange(7, -1, -1)]
+            bits = [(char >> i) & 1 for i in range(7, -1, -1)]
             # list of positions with ones
             set_bits = list(pos for pos, val in enumerate(bits) if val)
             # yield everything we need
@@ -340,7 +345,7 @@ class YearEvents(GenericPeriodEvents):
         self.system = system
 
         months = []
-        for m in xrange(1, 13):
+        for m in range(1, 13):
             months.append( MonthEvents(event_name, self.year, m, system) )
         or_op = BitOpOr(*months)
         self.redis_key = or_op.redis_key

setup.py
@@ -12,13 +12,23 @@ setup(name='bitmapist',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
-      install_requires = ['redis>=2.10.0', 'python-dateutil'],
+      install_requires=[
+          'redis>=2.10.0',
+          'python-dateutil',
+          'future>=0.14.3'
+      ],
       classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
       ],
       packages=['bitmapist', 'bitmapist.cohort'],

.gitignore
@@ -2,3 +2,4 @@
 *.pyc
 *.egg-info
 /dist
+.tox

.travis.yml
@@ -1,11 +1,20 @@
 language: python
-python:
-  - "2.7"
+
+sudo: false
 
 services:
   - redis-server
 
-install:
-  - python setup.py -q install
+env:
+  - TOX_ENV=py27
+  - TOX_ENV=py32
+  - TOX_ENV=py33
+  - TOX_ENV=py34
+  - TOX_ENV=pypy
+  - TOX_ENV=pypy3
 
-script: py.test -vv
+install:
+  - pip install tox
+
+script:
+  - tox -e $TOX_ENV

test/conftest.py
@@ -1,3 +1,5 @@
+from builtins import range
+
 import pytest
 import os
 import subprocess
@@ -65,7 +67,7 @@ def wait_for_socket(host, port, seconds=3):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.1)
-    for _ in xrange(iterations):
+    for _ in range(iterations):
         result = sock.connect_ex((host, port))
         if result == 0:
             sock.close()

tox.ini
@@ -1,0 +1,12 @@
+[tox]
+envlist = py{27,32,33,34},
+          pypy
+          pypy3
+
+
+[testenv]
+commands =
+  py.test -vv
+
+deps =
+    pytest

commit c7727ad241acacc3c1a9e0c2526679ace15d29a1

Speed up iterations over items in MixinIter
Essentially, a fix for a regression accidentally introduced in 2ab8e16

Refs #30

bitmapist/__init__.py
@@ -310,10 +310,9 @@ class MixinIter:
 
         val = bytes(val)
 
-        zero = chr(0)
         for char_num, char in enumerate(val):
             # shortcut
-            if char == zero:
+            if char == 0:
                 continue
             # find set bits, generate smth like [1, 0, ...]
             bits = [(char >> i) & 1 for i in range(7, -1, -1)]

setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='bitmapist',
-      version='3.100',
+      version='3.101',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",