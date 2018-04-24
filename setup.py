bcf89e00ee647d414c0adde481700b91e5ee5308
Amir Salihefendic <amix@doist.com>
2018-04-24 11:15:40 +0200

    Update setup.py
    Removed an unfortunate copyright copy-paste

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-# Copyright (c) 2007 Qtrac Ltd. All rights reserved.
-# This module is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at
-# your option) any later version.
 
 from setuptools import setup
 
