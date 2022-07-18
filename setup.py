04babe58e4a50a94364ccdc1991256550f654cce
Amir <amix@doist.com>
2022-07-18 12:29:33 +0200

    Get ready to release 3.105

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='bitmapist',
-      version='3.104',
+      version='3.105',
       author="amix",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
